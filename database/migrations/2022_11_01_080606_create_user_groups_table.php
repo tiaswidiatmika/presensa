9bd79204daa0dba41f8368918ea940c70bd70841
tiaswidiatmika <tiaswidiatmika@gmail.com>
2022-11-01 16:09:45 +0800

    initial commit

---
+++ database/migrations/2022_11_01_080606_create_user_groups_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('codename');
+            $table->integer('first_day_of_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_groups');
+    }
+};
